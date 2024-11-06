a3415bcb8620b807a6d2b0451c026d773abab039
Mohamed_Alaa <m7md3laa500@gmail.com>
2024-11-06 03:42:09 +0200

    finish quran_tap

--- lib/screens/quran_tap/item_sura_name.dart
+++ lib/screens/quran_tap/item_sura_name.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:islami/screens/quran_tap/quran_tap.dart';
-import 'package:islami/screens/quran_tap/sura_details_screen.dart';
 
 class ItemSuraName extends StatelessWidget {
   final SuraData data;
@@ -10,30 +9,30 @@ class ItemSuraName extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Row(children: [
-        Expanded(
-          child: Text(
-            textAlign: TextAlign.center,
-            data.suraNumber,
-            style: GoogleFonts.amiri(
-                textStyle: Theme.of(context).textTheme.titleMedium),
-          ),
+    return Row(children: [
+      Expanded(
+        child: Text(
+          textAlign: TextAlign.center,
+          data.suraNumber,
+          style: GoogleFonts.amiri(
+              textStyle: Theme.of(context).textTheme.titleMedium),
         ),
-        SizedBox(
-          height: 40,
-          child: VerticalDivider(
-            color: Theme.of(context).primaryColor,
-            thickness: 2,
-          ),
+      ),
+      SizedBox(
+        height: 40,
+        child: VerticalDivider(
+          color: Theme.of(context).primaryColor,
+          thickness: 2,
         ),
-        Expanded(
-          child: Text(
-            textAlign: TextAlign.center,
-            data.suraName,
-            style: GoogleFonts.amiri(
-                textStyle: Theme.of(context).textTheme.titleMedium),
-          ),
+      ),
+      Expanded(
+        child: Text(
+          textAlign: TextAlign.center,
+          data.suraName,
+          style: GoogleFonts.amiri(
+              textStyle: Theme.of(context).textTheme.titleMedium),
         ),
-      ]);
+      ),
+    ]);
   }
 }
